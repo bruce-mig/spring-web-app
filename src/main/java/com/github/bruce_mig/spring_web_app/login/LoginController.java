576ab6d03105cf4d37f5580fb890a6c0e6f94846
bruce-mig <brucetmig@icloud.com>
2024-06-03 20:04:10 +0200

    added welcome page

--- src/main/java/com/github/bruce_mig/spring_web_app/login/LoginController.java
+++ src/main/java/com/github/bruce_mig/spring_web_app/login/LoginController.java
@@ -5,17 +5,32 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
+    private final AuthenticationService authenticationService;
+
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
-    @RequestMapping("login")
-    public String gotoLoginPage(@RequestParam String name, ModelMap model) {
-        model.put("name", name);
-        logger.debug("Request param is {}",name);
+    public LoginController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @RequestMapping(value="login", method = RequestMethod.GET)
+    public String gotoLoginPage() {
+        return "login";
+    }
+
+    @RequestMapping(value="login", method = RequestMethod.POST)
+    public String gotoWelcomePage(@RequestParam String name, @RequestParam String password, ModelMap model) {
+        if (authenticationService.authenticate(name, password)){
+            model.put("name", name);
+            return "welcome";
+        }
+        model.put("errorMessage", "Invalid Credentials! Please try again.");
         return "login";
     }
 }
