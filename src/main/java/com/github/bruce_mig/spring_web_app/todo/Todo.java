53abebb71fd3af95cca4a1883b97d28957950758
bruce-mig <brucetmig@icloud.com>
2024-06-04 08:06:06 +0200

    using jstl

--- src/main/java/com/github/bruce_mig/spring_web_app/todo/Todo.java
+++ src/main/java/com/github/bruce_mig/spring_web_app/todo/Todo.java
@@ -2,11 +2,69 @@ package com.github.bruce_mig.spring_web_app.todo;
 
 import java.time.LocalDate;
 
-public record Todo(
-        Integer id,
-        String username,
-        String description,
-        LocalDate targetDate,
-        boolean done
-) {
+public class Todo {
+     private Integer id;
+     private String username;
+     private String description;
+     private LocalDate targetDate;
+     private Boolean done;
+
+    public Todo(Integer id, String username, String description, LocalDate targetDate, Boolean done) {
+        this.id = id;
+        this.username = username;
+        this.description = description;
+        this.targetDate = targetDate;
+        this.done = done;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDate getTargetDate() {
+        return targetDate;
+    }
+
+    public Boolean getDone() {
+        return done;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setTargetDate(LocalDate targetDate) {
+        this.targetDate = targetDate;
+    }
+
+    public void setDone(Boolean done) {
+        this.done = done;
+    }
+
+    @Override
+    public String toString() {
+        return "Todo{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", description='" + description + '\'' +
+                ", targetDate=" + targetDate +
+                ", done=" + done +
+                '}';
+    }
 }
