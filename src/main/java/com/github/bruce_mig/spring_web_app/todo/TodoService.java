7e45713d347e7400bde51d319c3da5103db45913
bruce-mig <brucetmig@icloud.com>
2024-06-18 15:03:57 +0200

    added delete api

--- src/main/java/com/github/bruce_mig/spring_web_app/todo/TodoService.java
+++ src/main/java/com/github/bruce_mig/spring_web_app/todo/TodoService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 @Service
 public class TodoService {
@@ -22,5 +23,10 @@ public class TodoService {
     public void addTodo(String username, String description, LocalDate target, boolean done) {
         Todo todo = new Todo(++todosCount,username,description,target,done);
         todos.add(todo);
+    }
+
+    public void deleteById(int id){
+        Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+        todos.removeIf(predicate);
     }
 }
