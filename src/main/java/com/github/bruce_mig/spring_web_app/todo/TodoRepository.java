261a2b22998255d34e2948476a59e52d3aef1259
bruce-mig <brucetmig@icloud.com>
2024-06-20 10:39:51 +0200

    created repository interface

---
+++ src/main/java/com/github/bruce_mig/spring_web_app/todo/TodoRepository.java
@@ -0,0 +1,11 @@
+package com.github.bruce_mig.spring_web_app.todo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TodoRepository extends JpaRepository<Todo, Integer> {
+    public List<Todo> findByUsername(String username);
+}
