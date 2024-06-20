4833fa26feceee02aa3a6f84e5fb9ad656a68bd5
bruce-mig <brucetmig@icloud.com>
2024-06-20 10:38:46 +0200

    made Todo an entity

--- src/main/java/com/github/bruce_mig/spring_web_app/todo/Todo.java
+++ src/main/java/com/github/bruce_mig/spring_web_app/todo/Todo.java
@@ -1,73 +1,26 @@
 package com.github.bruce_mig.spring_web_app.todo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Size;
+import lombok.*;
 
 import java.time.LocalDate;
 
+@Entity
+@Setter
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class Todo {
+    @Id
+    @GeneratedValue
      private Integer id;
      private String username;
      @Size(min=5,message = "Enter at least 5 characters")
      private String description;
      private LocalDate targetDate;
      private Boolean done;
-
-    public Todo(Integer id, String username, String description, LocalDate targetDate, Boolean done) {
-        this.id = id;
-        this.username = username;
-        this.description = description;
-        this.targetDate = targetDate;
-        this.done = done;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public LocalDate getTargetDate() {
-        return targetDate;
-    }
-
-    public Boolean getDone() {
-        return done;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setTargetDate(LocalDate targetDate) {
-        this.targetDate = targetDate;
-    }
-
-    public void setDone(Boolean done) {
-        this.done = done;
-    }
-
-    @Override
-    public String toString() {
-        return "Todo{" +
-                "id=" + id +
-                ", username='" + username + '\'' +
-                ", description='" + description + '\'' +
-                ", targetDate=" + targetDate +
-                ", done=" + done +
-                '}';
-    }
 }
