4cbe5fe23f7274817825b40a205d8a16c4482f36
bruce-mig <brucetmig@icloud.com>
2024-06-20 09:20:40 +0200

    added custom security filter chain

--- src/main/java/com/github/bruce_mig/spring_web_app/security/SpringSecurityConfiguration.java
+++ src/main/java/com/github/bruce_mig/spring_web_app/security/SpringSecurityConfiguration.java
@@ -1,12 +1,17 @@
 package com.github.bruce_mig.spring_web_app.security;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.function.Function;
 
@@ -36,5 +41,25 @@ public class SpringSecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    /*
+    * Spring security
+    *  All URLs are protected
+     * A login form is shown for unauthorized users
+     * CSRF is disabled
+     * Frames (used by h2 console) are disabled
+     */
+
+    // SecurityFilterChain - Defines a filter chain matched against every request
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated())
+                .formLogin(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
+        return http.build();
     }
 }
